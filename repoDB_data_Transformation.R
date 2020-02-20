e3aef2be2e6216559e0ad7afdd70715162cc9f4f
Akmazad <a.azad@monash.edu>
2020-02-20 19:38:26 +1100

    Corrected

--- repoDB_data_Transformation.R
+++ repoDB_data_Transformation.R
@@ -2,28 +2,32 @@ library(data.table)
 library(tidyr)
 library(purrr)
 library(pbapply)
+library(reshape2)
+
 repoDB.dat <- fread("Data/repoDB_full.csv")
 repoDB.dat <- select(repoDB.dat, c("drug_id", "ind_id", "status"))
 repoDB.dat$status = ifelse(repoDB.dat$status == "Approved", 1, 0)
-repoDB.dat = tidyr::unite(repoDB.dat, "Ind_Status", c("ind_id", "status"), sep="__")
+repoDB.dat = tidyr::unite(repoDB.dat, "Ind_Status", c("ind_id", "status"), sep="_")
 newDat <- split(repoDB.dat$Ind_Status, repoDB.dat$drug_id)
 
 findStatusofaPair <- function(x,y){
-  d1 = data.frame(info=x) %>% 
-        tidyr::separate(1,c("ind_id","status"), sep="__")
-  # print(d1)
-  d2 = data.frame(info=y) %>% 
-    tidyr::separate(1,c("ind_id","status"), sep="__")
-  # print(d2)
-  com = dplyr::inner_join(d1,d2, by="ind_id")
-  # print(com)
-  if(nrow(com) > 0){
-    tp_indx = which((com[,2] == 1) && (com[,3] == 1))
-    if(length(tp_indx) != 0)
-      return(1)
-    else
-      return(0)
+  commonOnes <- intersect(x,y)
+  if(length(commonOnes) > 0 & any(endsWith(commonOnes, "_1"))){
+    return(1) # TP: If both drugs are approved for the same indication(s)
+  }else{
+    return(0) # TN: Otherwise
   }
 }
+repoMat <- pbsapply(newDat, function(x) sapply(newDat, findStatusofaPair, x)) # repoMat is an adjacency matrix (i.e. binary)
 
-repoMat<- sapply(newDat[1:10], function(x) sapply(newDat[1:10], findStatusofaPair, x))
+repoAdjList <- melt(repoMat) # adjacency matrix to adjacency list (columns: Var1, Var2, value)
+repoAdjList <- filter(repoAdjList, value == 1)  # for some reason pipe (%>%) isn't working in my computer
+repoAdjList <- filter(repoAdjList, Var1 != Var2)
+
+# ----- get distinct pair only
+repoAdjList[1:2] = t(apply(repoAdjList[1:2],1,sort))
+repoAdjList = repoAdjList[!duplicated(repoAdjList[1:2]),]
+colnames(repoAdjList) <- c("DrugBank.ID1", "DrugBank.ID1", "repoDB_status")
+
+# ----- write into the file
+fwrite(repoAdjList, file = "Data/repoDB_drugPair_Transformation.csv")
